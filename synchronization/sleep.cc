93d6dcd9913f3c912f923a4984336a4d888a6535
Michael Goderbauer <goderbauer@google.com>
2017-03-30 16:37:55 -0700

    Port to Windows (round three)
    Change-Id: I044b2326ca4a83a0f20816175bc0f4f124036cb0

--- synchronization/sleep.cc
+++ synchronization/sleep.cc
@@ -4,13 +4,27 @@
 
 #include "lib/ftl/synchronization/sleep.h"
 
+#include "lib/ftl/build_config.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+#else
 #include <time.h>
+#endif // defined(OS_WIN)
 
 namespace ftl {
 
 void SleepFor(TimeDelta duration) {
+#if defined(OS_WIN)
+  LARGE_INTEGER duration_time;
+  duration_time.QuadPart = (duration.ToNanoseconds() / 100) * -1;
+  HANDLE timer = CreateWaitableTimer(NULL, TRUE, NULL);
+  SetWaitableTimer(timer, &duration_time, 0, NULL, NULL, 0);
+  WaitForSingleObject(timer, INFINITE);
+#else
   struct timespec ts = duration.ToTimespec();
   nanosleep(&ts, nullptr);
+#endif // defined(OS_WIN)
 }
 
 }  // namespace ftl
